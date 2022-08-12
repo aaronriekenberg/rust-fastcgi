e0afa7622ef007c4e2fee55eb96c9683f4b16dc5
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2022-08-11 19:56:00 -0500

    Add handlers module.

--- src/main.rs
+++ src/main.rs
@@ -1,176 +1,21 @@
 mod config;
+mod handlers;
 
-use std::collections::BTreeMap;
-use std::fmt::{Debug, Write};
+use std::fmt::Write;
 use std::sync::Arc;
 
-use async_trait::async_trait;
+use crate::handlers::RequestHandler;
 
 use log::{debug, error, info, warn};
 
 use tokio::net::{unix::OwnedWriteHalf, UnixListener};
 
-use tokio::process::Command;
-
 use tokio_fastcgi::{Request, RequestResult, Requests};
-
-use serde::Serialize;
-
-type HttpResponse = http::Response<Option<String>>;
-
-#[async_trait]
-trait RequestHandler: Send + Sync {
-    async fn handle(&self, request: Arc<Request<OwnedWriteHalf>>) -> HttpResponse;
-}
-
-fn build_json_response(response_dto: impl Serialize) -> HttpResponse {
-    let json_result = serde_json::to_string(&response_dto);
-
-    match json_result {
-        Err(e) => {
-            warn!("json serialization error {}", e);
-
-            http::Response::builder()
-                .status(http::StatusCode::INTERNAL_SERVER_ERROR)
-                .body(None)
-                .unwrap()
-        }
-        Ok(json_string) => http::Response::builder()
-            .status(http::StatusCode::OK)
-            .header(http::header::CONTENT_TYPE, "application/json")
-            .body(Some(json_string))
-            .unwrap(),
-    }
-}
-
-fn build_status_code_response(status_code: http::StatusCode) -> HttpResponse {
-    http::Response::builder()
-        .status(status_code)
-        .body(None)
-        .unwrap()
-}
-
-#[derive(Debug, Default, Serialize)]
-struct DebugResponse {
-    http_headers: BTreeMap<String, String>,
-    other_params: BTreeMap<String, String>,
-}
-
-struct DebugHandler {}
-
-#[async_trait]
-impl RequestHandler for DebugHandler {
-    async fn handle(&self, request: Arc<Request<OwnedWriteHalf>>) -> HttpResponse {
-        let mut debug_response = DebugResponse::default();
-
-        if let Some(str_params) = request.str_params_iter() {
-            for param in str_params {
-                let value = param.1.unwrap_or("[Invalid UTF8]").to_string();
-
-                let lower_case_key = param.0.to_ascii_lowercase();
-                if lower_case_key.starts_with("http_") {
-                    let http_header_key = &lower_case_key[5..];
-                    debug_response
-                        .http_headers
-                        .insert(http_header_key.to_string(), value);
-                } else {
-                    debug_response.other_params.insert(lower_case_key, value);
-                }
-            }
-        }
-
-        build_json_response(debug_response)
-    }
-}
-
-#[derive(Debug, Default, Serialize)]
-struct CommandResponse {
-    command_output: String,
-}
-
-struct CommandHandler {}
-
-#[async_trait]
-impl RequestHandler for CommandHandler {
-    async fn handle(&self, _request: Arc<Request<OwnedWriteHalf>>) -> HttpResponse {
-        let command_result = Command::new("ls").arg("-latrh").output().await;
-
-        let output = match command_result {
-            Err(err) => {
-                let command_response = CommandResponse {
-                    command_output: format!("error running command {}", err),
-                };
-                return build_json_response(command_response);
-            }
-            Ok(output) => output,
-        };
-
-        let mut combined_output = String::with_capacity(output.stderr.len() + output.stdout.len());
-        combined_output.push_str(&String::from_utf8_lossy(&output.stderr));
-        combined_output.push_str(&String::from_utf8_lossy(&output.stdout));
-
-        let command_response = CommandResponse {
-            command_output: combined_output,
-        };
-
-        build_json_response(command_response)
-    }
-}
-
-struct Route {
-    url_prefix: String,
-    request_handler: Box<dyn RequestHandler>,
-}
-
-impl Route {
-    fn matches(&self, request_uri: &str) -> bool {
-        request_uri.starts_with(&self.url_prefix)
-    }
-}
-
-struct Router {
-    routes: Vec<Route>,
-}
-
-impl Router {
-    fn new() -> Self {
-        let mut routes = Vec::new();
-
-        routes.push(Route {
-            url_prefix: "/cgi-bin/debug".to_string(),
-            request_handler: Box::new(DebugHandler {}),
-        });
-
-        routes.push(Route {
-            url_prefix: "/cgi-bin/commands/ls".to_string(),
-            request_handler: Box::new(CommandHandler {}),
-        });
-
-        Self { routes }
-    }
-}
-
-#[async_trait]
-impl RequestHandler for Router {
-    async fn handle(&self, request: Arc<Request<OwnedWriteHalf>>) -> HttpResponse {
-        if let Some(request_uri) = request.get_str_param("request_uri").map(String::from) {
-            for route in &self.routes {
-                if route.matches(&request_uri) {
-                    return route.request_handler.handle(request).await;
-                }
-            }
-
-            build_status_code_response(http::StatusCode::NOT_FOUND)
-        } else {
-            build_status_code_response(http::StatusCode::BAD_REQUEST)
-        }
-    }
-}
 
 /// Encodes the HTTP status code and the response string and sends it back to the webserver.
 async fn send_response(
     request: Arc<Request<OwnedWriteHalf>>,
-    response: HttpResponse,
+    response: crate::handlers::HttpResponse,
 ) -> Result<RequestResult, tokio_fastcgi::Error> {
     debug!("send_response response = {:?}", response);
 
@@ -229,7 +74,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     info!("listening on {:?}", listener.local_addr()?);
 
-    let router = Arc::new(Router::new());
+    let router = Arc::new(crate::handlers::Router::new());
 
     loop {
         let connection = listener.accept().await;
