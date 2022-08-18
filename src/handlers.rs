e8127a0034bebbedd58be4626b714aac85736811
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2022-08-17 19:11:33 -0500

    Use anonymous lifetime to simplify.

--- src/handlers.rs
+++ src/handlers.rs
@@ -43,7 +43,7 @@ pub type HttpResponse = http::Response<Option<String>>;
 
 #[async_trait]
 pub trait RequestHandler: Send + Sync {
-    async fn handle<'a>(&self, request: FastCGIRequest<'a>) -> HttpResponse;
+    async fn handle(&self, request: FastCGIRequest<'_>) -> HttpResponse;
 }
 
 fn build_json_response(response_dto: impl Serialize) -> HttpResponse {
@@ -96,7 +96,7 @@ impl RequestInfoHandler {
 
 #[async_trait]
 impl RequestHandler for RequestInfoHandler {
-    async fn handle<'a>(&self, request: FastCGIRequest<'a>) -> HttpResponse {
+    async fn handle(&self, request: FastCGIRequest<'_>) -> HttpResponse {
         let mut response = RequestInfoResponse {
             role: request.role(),
             connection_id: *request.connection_id(),
@@ -129,7 +129,7 @@ impl AllCommandsHandler {
 
 #[async_trait]
 impl RequestHandler for AllCommandsHandler {
-    async fn handle<'a>(&self, _request: FastCGIRequest<'a>) -> HttpResponse {
+    async fn handle(&self, _request: FastCGIRequest<'_>) -> HttpResponse {
         build_json_response(&self.commands)
     }
 }
@@ -154,7 +154,7 @@ impl RunCommandHandler {
 
 #[async_trait]
 impl RequestHandler for RunCommandHandler {
-    async fn handle<'a>(&self, _request: FastCGIRequest<'a>) -> HttpResponse {
+    async fn handle(&self, _request: FastCGIRequest<'_>) -> HttpResponse {
         let command_start_time = Instant::now();
 
         let command_result = Command::new(self.command_info.command())
@@ -215,7 +215,7 @@ impl Router {
 
 #[async_trait]
 impl RequestHandler for Router {
-    async fn handle<'a>(&self, request: FastCGIRequest<'a>) -> HttpResponse {
+    async fn handle(&self, request: FastCGIRequest<'_>) -> HttpResponse {
         if let Some(request_uri) = request.params().get("request_uri") {
             for route in &self.routes {
                 if route.matches(&request_uri) {
